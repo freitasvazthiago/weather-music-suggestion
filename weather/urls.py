1ac1bdc1889a88c152221542247bbbeb77f810ee
root <root@svnb029.softvision.local>
2020-08-25 14:48:50 -0300

    add config file, new requirements libs, change on get token from spotify

--- weather/urls.py
+++ weather/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('get', views.getSpotifyMusicPlaylist, name='getSpotifyMusicPlaylist'),
+    path('get', views.getSpotifyToken, name='getSpotifyToken'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,18 @@
 import requests
+import spotipy
+import spotipy.util as util
+import configparser
+import spotipy.oauth2 as oauth2
+
 from django.http import HttpResponse, JsonResponse
+from spotipy.oauth2 import SpotifyClientCredentials
 
-WEATHER_ENDPOINT = 'http://api.openweathermap.org/data/2.5/weather'
-WEATHER_APPID = '931a7af1736cc80e90ef7752a4b5d789'
 
-CLASSIC_PLAYLIST = '7umNJ28Wi1QQwllNMvRORu'
-ROCK_PLAYLIST = '6zJYd4fWSqRIR7g1Tp2lam'
-POP_PLAYLIST = '27BkrEIPh3FiCGfnaOtnOK'
-
-USER_ID = 'freitasvazthiago'
-TOKEN = 'BQBXqmyVOTEbcO-uF1633ReN9YIg1RoYhd2tIUzQhVyIGXaKr0CpifSisLMsPQIQNJel1cAUkH7sXMRFwPbRX9ljrPAPmd9gR5RNzVXXoOYIrLqXUVa7HfRk3-FHJWeJbdfFQDqOOqEvyVfGb6C2JeDP8K3Ew9MODS3D'
-
+config = configparser.ConfigParser()
 
 def index(request):
+    config.read('config.cfg')
+
     if request.method == 'GET':
         city_name = request.GET['city_name']
 
@@ -20,10 +20,10 @@ def index(request):
             'q': city_name,
             'lan': 'pt_br',
             'units': 'metric',
-            'appid': WEATHER_APPID
+            'appid': config.get('WEATHER', 'WEATHER_APPID')
         }
 
-        r = requests.get(WEATHER_ENDPOINT, params).json()
+        r = requests.get(config.get('WEATHER', 'WEATHER_ENDPOINT'), params).json()
 
     
     if r['cod'] != 200:
@@ -33,11 +33,11 @@ def index(request):
     musics = []
 
     if current_temperature > 25:
-        musics.append(getSpotifyMusicPlaylist(ROCK_PLAYLIST))
+        musics.append(getSpotifyPlaylistSongs(config.get('PLAYLIST', 'POP_PLAYLIST')))
     elif current_temperature >= 10 and current_temperature <= 25:
-        musics.append(getSpotifyMusicPlaylist(POP_PLAYLIST))
+        musics.append(getSpotifyPlaylistSongs(config.get('PLAYLIST', 'ROCK_PLAYLIST')))
     else:
-        musics.append(getSpotifyMusicPlaylist(CLASSIC_PLAYLIST))
+        musics.append(getSpotifyPlaylistSongs(config.get('PLAYLIST', 'CLASSIC_PLAYLIST')))
 
     return JsonResponse(
         {
@@ -52,16 +52,26 @@ def index(request):
         }
     )
 
+def getSpotifyPlaylistSongs(playlist):
 
-def getSpotifyMusicPlaylist(playlist):
-    url = 'https://api.spotify.com/v1/playlists/'+ playlist + '/tracks?fields=items(track(name))'
+    spotify = getSpotifyToken()
+    results = spotify.playlist_tracks(playlist, fields='items(track(name))')
 
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization' : 'Bearer ' + TOKEN
-    }
+    return results
 
-    r = requests.get(url, headers=headers).json()
-    
-    return r
+def getSpotifyToken():
+
+    config.read('config.cfg')
+
+    client_id = config.get('SPOTIFY', 'CLIENT_ID')
+    client_secret = config.get('SPOTIFY', 'CLIENT_SECRET')
+
+    auth = oauth2.SpotifyClientCredentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+
+    token = auth.get_access_token()
+    spotify = spotipy.Spotify(auth=token)
+
+    return spotify
